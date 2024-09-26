3cb98fa0c4d3bd75bfcafb443c5a05ab1e76b827
IstiyakCPL <istiyak.rahin@projukti-bd.com>
2024-09-26 12:17:45 +0600

    Backend Dashboard Mastaring

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('backend.dashboard');
 });
 
 Route::get('/hello', function() {
