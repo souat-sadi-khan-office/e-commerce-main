0259726e0988e25fef81890ad37447e061cfbd76
IstiyakCPL <istiyak.rahin@projukti-bd.com>
2024-10-22 11:23:10 +0600

    Database Necessary Table Migrations  for Creating Orders

---
+++ app/Models/OrderDetail.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderDetail extends Model
+{
+    protected $fillable = [
+        'order_id',
+        'product_ids', // Assuming this is a string of IDs or JSON
+        'details',     // Assuming this is a JSON
+        'notes',
+        'shipping_method',
+        'shipping_address',
+        'billing_address',
+    ];
+
+    // Define the relationships
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class, 'order_id');
+    }
+}
